ce90cd2e60c097844c26e6482e0654a2caea7295
Demchyshyn <demchyshyn.v87@gmail.com>
2021-03-04 23:04:49 +0200

    django lesson2

---
+++ users/url.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import ListCreateView,ReadUpdateDeleteView
+urlpatterns = [
+    path('', ListCreateView.as_view()),
+    path('/<int:pk>', ReadUpdateDeleteView.as_view())
+]
